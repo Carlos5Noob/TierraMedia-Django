ba9865e724f6eaf36a438419d47bcd38c4afd882
JonatanGarLun <jgarlun2112@fp.iesromerovargas.com>
2025-02-03 11:52:49 +0100

    Modelos añadidos al admin

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from app.models import Personaje, Arma
+
 # Register your models here.
+
+admin.site.register(Personaje)
+admin.site.register(Arma)
--- app/models.py
+++ app/models.py
@@ -2,13 +2,17 @@ from django.db import models
 
 # Create your models here.
 
-class User(models.Model):
+class Personaje(models.Model):
     nombre = models.CharField(max_length=100)
     salud = models.IntegerField(default=200)
 
-
+    def __str__(self):
+        return self.nombre
 
 class Arma(models.Model):
     nombre = models.CharField(max_length=100)
-    dano = models.IntegerField()
-    critico = models.IntegerField()
+    dano = models.IntegerField(default=0)
+    critico = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.nombre
