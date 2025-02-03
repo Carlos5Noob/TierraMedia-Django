731f979d2795267bc84f5d0e1fbba8d7d0868ae5
Carlos5Noob <cchaati2101@fp.iesromerovargas.com>
2025-02-03 11:57:12 +0100

    3 vistas principales añadidas (+ configurado en las urls). Vistas basadas en clases

---
+++ app/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path, include
+from app import views
+
+urlpatterns = [
+    path('', views.HomeView.as_view(), name='index'),
+    path('personajes/', views.ListCharacters.as_view(), name='personajes'),
+    path('armas/', views.ListWeapons.as_view(), name='armas'),
+]
