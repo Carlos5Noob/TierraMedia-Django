0c28fbf90ce16deae511b6bd9f93de3d53ed1236
JonatanGarLun <jgarlun2112@fp.iesromerovargas.com>
2025-02-03 10:51:43 +0100

    Primeros modelos

--- app/models.py
+++ app/models.py
@@ -2,16 +2,13 @@ from django.db import models
 
 # Create your models here.
 
-class Arma(models.Model):
-    nombre = models.CharField(max_length=50)
-    potencia = models.IntegerField()
-
-    def __str__(self):
-        return self.nombre
-
-class Personaje(models.Model):
+class User(models.Model):
     nombre = models.CharField(max_length=100)
-    arma = models.ManyToManyField(Arma)
+    salud = models.IntegerField(default=200)
 
-    def __str__(self):
-        return self.nombre
+
+
+class Arma(models.Model):
+    nombre = models.CharField(max_length=100)
+    dano = models.IntegerField()
+    critico = models.IntegerField()
