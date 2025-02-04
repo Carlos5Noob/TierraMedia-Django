3d1c6410b6904c84b0c50ac435c4f6abea3f44a3
Alvarokstar <afercal1704@fp.iesromerovargas.com>
2025-02-04 10:34:15 +0100

    nuevos registros en el admin

--- app/admin.py
+++ app/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Personaje, Arma, Faccion
+from .models import Personaje, Arma, Faccion, Ubicacion
 
 # Register your models here.
 
 admin.site.register(Personaje)
 admin.site.register(Arma)
 admin.site.register(Faccion)
+admin.site.register(Ubicacion)
