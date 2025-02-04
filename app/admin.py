8754e2554b16046e4a570fe1749576bfeffd0efb
JonatanGarLun <jgarlun2112@fp.iesromerovargas.com>
2025-02-04 10:03:40 +0100

    Añadido combate al admin

--- app/admin.py
+++ app/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Personaje, Arma
+from .models import Personaje, Arma, Combate
 
 # Register your models here.
 
 admin.site.register(Personaje)
 admin.site.register(Arma)
+admin.site.register(Combate)
