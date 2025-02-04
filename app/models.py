28e6753249ad1119a5c55c0574e7678c5fe30bca
Carlos5Noob <cchaati2101@fp.iesromerovargas.com>
2025-02-04 08:27:15 +0100

    bug bd arreglado

--- app/models.py
+++ app/models.py
@@ -4,15 +4,15 @@ from django.db import models
 
 class Arma(models.Model):
     nombre = models.CharField(max_length=100)
-    dano = models.IntegerField(default=0)
-    critico = models.IntegerField(default=0)
+    dano = models.IntegerField()
+    critico = models.IntegerField()
 
     def __str__(self):
         return self.nombre
 
 class Personaje(models.Model):
     nombre = models.CharField(max_length=100)
-    salud = models.IntegerField(default=200)
+    salud = models.IntegerField()
     arma = models.ForeignKey(Arma, on_delete=models.CASCADE)
 
     def __str__(self):
