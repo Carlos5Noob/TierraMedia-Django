07a15010d9fce4f8066f518bf79bc777a848dd15
JonatanGarLun <jgarlun2112@fp.iesromerovargas.com>
2025-02-04 09:18:30 +0100

    Más avances en el formulario de combates y creada su url

--- app/urls.py
+++ app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='index'),
     path('personajes/', views.ListCharacters.as_view(), name='personajes'),
     path('armas/', views.ListWeapons.as_view(), name='armas'),
+    path('seleccion-pj/', views.FormList.as_view(), name='seleccion-pj'),
 ]
