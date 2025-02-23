1ff24e8e9df8630f256a149b4bef5093b3c6f48e
Carlos5Noob <cchaati2101@g.educaand.es>
2025-02-23 01:29:26 +0100

    añadido ruta static para recojer archivos del media/ (me quiero arrancar los huevos x'D)

--- tierramedia/urls.py
+++ tierramedia/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('game/', include("app.urls")), # el path donde se incluyen las urls de la aplicación
     path('tierramedia/', include("django.contrib.auth.urls")), # el path donde se incluyen las urls de autenticación
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
