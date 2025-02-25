63c01f47a41f632c915b26bb7375c4a96a49a2bb
JonatanGarLun <jgarlun2112@fp.iesromerovargas.com>
2025-02-25 10:40:45 +0100

    Cambios en los settings.py

--- tierramedia/settings.py
+++ tierramedia/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app.apps.AppConfig',
-
     'api.apps.ApiConfig',
     'rest_framework',
     'rest_framework.authtoken',
